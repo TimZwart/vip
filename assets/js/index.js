192e5aa300be609acab54ff22964d4da28d85e0b
Tim Zwart <timwaagh@gmail.com>
2016-06-13 04:28:29 +0200

    lots of things have changed yet more have stayed the same. it looks better but is still too static

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,23 +3,28 @@ var navbar_to_html = function(nav_json){
         var menus = nav_json.menus;
         var menu_to_html = function(menu_json) {
                 var link_html = function(link_json) {
-                      return '<li><a href="'+link_json.href+'">'+link_json.descr+'</a>'; 
-                }
-                var links_html = menu_json.map(link_html).reduce(add_two_things);
-                return links_html;
+                      return '<li><a href="'+link_json.href+'">'+link_json.descr+'</a></li>'; 
+                };
+		var links = menu_json.items.map(link_html);
+                var innerhtml = '<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">'+menu_json.descr+'<span class="caret"></span></a><ul class="dropdown-menu">'+links.reduce(add_two_things)+'</ul>';
+		var html = '<li class="dropdown">'+innerhtml+'</li>';
+                return html;
         };
-        return menus.map(menu_to_html).reduce(add_two_things);
+	var lis = menus.map(menu_to_html);
+	var html = lis.reduce(add_two_things);
+	return html;
 };
 var process_page_infos= function(resp){
         var resp = JSON.parse(resp);
         document.title = resp.title;
         $("h1.heading").text(resp.heading);
         $(".blog_description").text(resp.blog_description);
-        $("#nav ul").html(navbar_to_html(resp.nav));
+        $("#navbar ul").html(navbar_to_html(resp.nav));
 };
 $.get(uri_root+"/"+controller+"/page_info", process_page_infos);
 post_to_html = function(post) {
-        return '<div class="row"><div class="post"><h2 class="blog_post_title">'+post.title+'</h2>'+post.content+'<ul class="comments"></ul></div></div>';
+        return '<div class="post" data-id="'+post.id+'><div class="row" "><h2 class="blog_post_title">'+post.title+'</h2>'+post.content+'</div>'+
+		'<div class="row"><button class="btn btn-default comments_show">show comments</button><div class="comment_section"><ul class="comments"></ul><form class="comment_form"><input type="text" name="comment_subject"><input name="comment_content" type="text"><input type="submit" value="post comment"></form></div></div></div>';
 };
 var process_posts = function(resp){
         var posts = JSON.parse(resp);
@@ -29,3 +34,35 @@ var process_posts = function(resp){
         $("ul.posts").html(posts_html);
 };
 $.get(uri_root+"/"+controller+"/posts", process_posts);
+var comments_to_html = function(div, callb){
+	var comment_to_html = function(comment_json) {
+		return '<li><h3>'+comment_json.title+'</h3>'+comment_json.text+'</li>';
+	};
+	var process_comments = function(comments_json){
+		comments_arr = JSON.parse(comments_json);
+		var comments_html = comments_arr.map(comment_to_html);
+		$(div).find("ul.comments").html(comments_html);
+		callb();
+	};
+	$.post(uri_root+"/"+controller+"/get_comments/"+$(div).data('id'), process_comments);
+	
+};
+var show_comments = function(e) {
+	var comment_section = $(this).siblings('div.comment_section');
+	comments_to_html( $(this).parents('div.post'), function(){
+		comment_section.show();
+	});
+}
+$("ul.posts").on("click", ".comments_show", show_comments);
+var submit_comment = function(e){
+	e.preventDefault();
+	var data = $(this).serializeArray();
+	var div_post = $(this).parents('div.post');
+	var postid = div_post.data('id');
+	data[data.length] =  { name: "post_id", value: post_id };
+	var renewcomments = function() {
+		comments_to_html(div_post);
+	};
+	$.post(uri_root+"/"+controller+"/post_comment", data, renewcomments);
+};
+$("ul.posts").on("submit", "comment_form", submit_comment);
--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -12,10 +12,21 @@ class DefaultController extends Controller{
                 $pageinfo = [   "title" => "hoi",
                                 "heading" => "hallo",
                                 "blog_description" => "dit is een weblog",
-                                "nav" => ["menus" => ["menu1" => ["fadda" => "http://fadda.com"]]]
+                                "nav" => ["menus" => [ ["items" => [["url" => "http://fadda.com", "descr" => "fadda"], ["url" => "http://google.com", "descr" => "google"]], "descr" => "links" ]], "descr" => "hoi"]
                 ];
                 $this->loadAPIView("pageinfo", $pageinfo);
         }
-}
-
+	public function post_comment(){
+		$title = $_POST['title'];
+		$text = $_POST['text'];
+		$post_id = $_POST['post_id'];
+		$query = 'insert into comments (post_id, title, text) values (?, ?, ?)';
+		(self::$db)->query($query, [$post_id, $title, $text]);
+	}
+	public function get_comments($post_id){
+		$query = 'select * from comments where post_id = ?';
+		$comments = (self::$db)->query($query, [$post_id]);
+		$this->loadAPIView($comments);
+	}
+} 
 ?>
